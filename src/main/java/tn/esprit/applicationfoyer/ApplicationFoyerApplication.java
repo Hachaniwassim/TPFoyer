e95d52fd90a257fb992dd63e83410eeaaa6fa985
hachaniwassim <wassimhach16@gmail.com>
2023-09-21 00:27:28 +0100

    Installing Dependencies and fixing project

---
+++ src/main/java/tn/esprit/applicationfoyer/ApplicationFoyerApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.applicationfoyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationFoyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationFoyerApplication.class, args);
+    }
+
+}
