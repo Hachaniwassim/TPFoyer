d3696079385dae95e241fd120e62dd2454a37262
hachaniwassim <wassimhach16@gmail.com>
2023-11-21 09:44:17 +0100

    Using Spring Scheduler in our project

--- src/main/java/tn/esprit/applicationfoyer/control/ChambreRestController.java
+++ src/main/java/tn/esprit/applicationfoyer/control/ChambreRestController.java
@@ -1,5 +1,7 @@
 package tn.esprit.applicationfoyer.control;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tn.esprit.applicationfoyer.Entity.Chambre;
@@ -7,6 +9,7 @@ import tn.esprit.applicationfoyer.service.IChambreService;
 
 import java.util.List;
 
+@Tag(name = "Gestion Chambre")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/chambre")
@@ -14,6 +17,7 @@ public class ChambreRestController {
     IChambreService chambreService;
 
     // http://localhost:8089/tpfoyer/chambre/retrieve-all-chambres
+    @Operation(description = "récupérer toutes les chambres de la base de données")
     @GetMapping("/retrieve-all-chambres")
     public List<Chambre> getChambres() {
         List<Chambre> listChambres = chambreService.retrieveAllChambres();
@@ -21,6 +25,7 @@ public class ChambreRestController {
     }
 
     // http://localhost:8089/tpfoyer/chambre/retrieve-chambre/8
+    @Operation(description = "récupérer une chambre selon son id de la base de données")
     @GetMapping("/retrieve-chambre/{chambre-id}")
     public Chambre retrieveChambre(@PathVariable("chambre-id") Long chId) {
         Chambre chambre = chambreService.retrieveChambre(chId);
@@ -28,17 +33,20 @@ public class ChambreRestController {
     }
 
     // http://localhost:8089/tpfoyer/chambre/add-chambre
+    @Operation(description = "Ajouter une chambre dans la base de données")
     @PostMapping("/add-chambre")
     public Chambre addChambre(@RequestBody Chambre c) {
         Chambre chambre = chambreService.addChambre(c);
         return chambre;
     }
     // http://localhost:8089/tpfoyer/chambre/remove-chambre/{chambre-id}
+    @Operation(description = "Supprimer une chambre selon son id de la base de données")
     @DeleteMapping("/remove-chambre/{chambre-id}")
     public void removeChambre(@PathVariable("chambre-id") Long chId) {
         chambreService.removeChambre(chId);
     }
     // http://localhost:8089/tpfoyer/chambre/modify-chambre
+    @Operation(description = "Modifier une chambre dans la base de données")
     @PutMapping("/modify-chambre")
     public Chambre modifyChambre(@RequestBody Chambre c) {
         Chambre chambre = chambreService.modifyChambre(c);
--- src/main/java/tn/esprit/applicationfoyer/service/BlocServiceImpl.java
+++ src/main/java/tn/esprit/applicationfoyer/service/BlocServiceImpl.java
@@ -1,6 +1,9 @@
 package tn.esprit.applicationfoyer.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import tn.esprit.applicationfoyer.Entity.Bloc;
 import tn.esprit.applicationfoyer.Repository.BlocRepository;
@@ -9,11 +12,21 @@ import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class BlocServiceImpl implements IBlocService {
 
     BlocRepository blocRepository;
+
+
+    @Scheduled(fixedDelay = 60000)
     public List<Bloc> retrieveAllBlocs() {
-        return blocRepository.findAll();
+
+        List<Bloc> ListB = blocRepository.findAll();
+        log.info("taille totale : " + ListB.size());
+        for (Bloc b: ListB) {
+            log.info("Bloc : " + b);
+        }
+        return ListB;
     }
     public Bloc retrieveBloc(Long idBloc) {
         return blocRepository.findById(idBloc).get();
